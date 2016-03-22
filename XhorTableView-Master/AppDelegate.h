7d35e37bb1e7060ebcf58f1b2c3f403de5602109
Xueyongwei <eoe_dev@icloud.com>
2016-03-22 17:04:28 +0800

    Initial Commit

---
+++ XhorTableView-Master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XhorTableView-Master
+//
+//  Created by xueyongwei on 16/3/22.
+//  Copyright © 2016年 xueyongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
